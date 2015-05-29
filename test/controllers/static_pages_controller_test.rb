7ce165446d0e34d231845c0506ad35ef90f3f018
Troy Hill <thill@troys-mac-mini.attlocal.net>
2015-05-28 20:24:51 -0400

    cleaned up

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,25 +5,50 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Mailed.com | Automatic SMS, Email, and Mail companion"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Mailed.com Assistance"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | About Mailed.com"
   end
 
-  test "Should get Contact" do
+  test "Should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Contact Mailed.com"
   end
+
+  test "Should get pricing" do
+    get :pricing
+    assert_response :success
+    assert_select "title", "Pricing | View our pricing"
+  end
+
+  test "Should get careers" do
+    get :careers
+    assert_response :success
+    assert_select "title", "Careers | Come work with us"
+  end
+
+  test "Should get products" do
+    get :products
+    assert_response :success
+    assert_select "title", "Products | Our product offerings"
+  end
+
+  test "Should get events" do
+    get :events
+    assert_response :success
+    assert_select "title", "Events | See our calendar"
+  end
+
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,9 +5,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", events_path
+    assert_select "a[href=?]", careers_path
+    assert_select "a[href=?]", pricing_path
+    assert_select "a[href=?]", products_path
+
   end
 end
